0f315c0678c59acf3487d3064bf55022d5f6f393
Epoch Wolf <epochwolf@gmail.com>
2011-12-17 14:46:19 -0800

    Adding indexes to migrations

--- db/migrate/20111216213129_create_literatures.rb
+++ db/migrate/20111216213129_create_literatures.rb
@@ -1,12 +1,15 @@
 class CreateLiteratures < ActiveRecord::Migration
   def change
     create_table :literatures do |t|
-      t.string :title
-      t.integer :user_id
-      t.text :contents
-      t.boolean :deleted
+      t.string :title,      :null => false
+      t.integer :user_id,   :null => false
+      t.text :contents,     :null => false
+      t.boolean :deleted,   :null => false, :default => false
 
       t.timestamps
     end
+    
+    add_index :literatures, [:deleted, :id]
+    add_index :literatures, :user_id
   end
 end

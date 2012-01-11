0e298738d86d1f60f4e373aa96d3dd4f270af7fb
Epoch Wolf <epochwolf@gmail.com>
2012-01-10 17:25:16 -0800

    Fix broken view, added admin? helper method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Controllers::SaveRecord
   include Controllers::Paged
   protect_from_forgery
-  helper_method :return_path, :here, :owner?
+  helper_method :return_path, :here, :owner?, :admin?
   
   def current_user_if_admin
     current_user.try(:admin?) ? current_user : nil
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
   
   def show403(message="This page is currently unavailable.")
     render :template => 'errors/403', :layout => nil, :status => 403, :locals => {:message => message}
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
   end
   
   def owner?(object)

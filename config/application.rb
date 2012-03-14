d36332ce0e4b25cd1bb26b413e8f4145082bab85
Epoch Wolf <epochwolf@gmail.com>
2012-03-13 21:56:49 -0700

    Updating production configuration to match a known working one.

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module LitSocial
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -22,7 +22,7 @@ preload_app true
 
 timeout 30
 
-listen APP_ROOT + "/tmp/unicorn.sock", :backlog => 64
+listen "127.0.0.1:3000"
 
 pid APP_ROOT + "/tmp/pids/unicorn.pid"
 

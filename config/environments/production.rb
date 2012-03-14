fc68beb9ee89f643f4e391fa795ae29bf2e7ee75
Epoch Wolf <epochwolf@gmail.com>
2012-03-13 22:06:04 -0700

    Fixing a missing precompiled asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ LitSocial::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css admin_editor.css active_admin.js admin_editor.js]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

3f45fbc74faeadd6709233407e71fb2961864d51
epochwolf <epochwolf@gmail.com>
2012-11-24 16:31:16 -0800

    Forgot to fix app root after moving unicorn config... GRRRR

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,4 @@
-APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+APP_ROOT = File.expand_path(File.dirname(File.dirname(File.dirname(__FILE__))))
 
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin

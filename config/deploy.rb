5bb358d905426048d03fbb400ee870eae5a72fbc
epochwolf <epochwolf@gmail.com>
2012-11-24 16:06:28 -0800

    Updating cap deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # All connections use public keys, anyone putting a password in here and committing it gets their password
 # login privileges permanently revoked.
 
-set :rvm_ruby_string, '1.9.2-p136@sf-final' # ruby environment for staging/production
+set :rvm_ruby_string, '1.9.3-p327@litsocial' # ruby environment for staging/production
 set :rvm_type, :system 
 require "rvm/capistrano"
 require "bundler/capistrano"
@@ -27,4 +27,33 @@ server "kazan.epochwolf.com", :app, :web, :db, :primary => true
 
 require 'capistrano-unicorn'
 
-after 'deploy:restart', 'unicorn:restart' # app IS NOT preloaded
+after 'deploy:restart', 'unicorn:restart'
+
+namespace :deploy do
+
+  namespace :shared do
+    folders = []
+    files = ["config/secrets.yml"]
+    
+    desc "Create the dirs in shared path."
+    task :setup do
+      folders.each do |folder|
+        run "mkdir -p  #{shared_path}/#{folder}"
+      end
+      files.each do |file|
+        run "mkdir -p #{shared_path}/#{File.dirname(file)} && touch #{shared_path}/#{file}"
+      end
+    end
+
+    desc "Link dir from shared to common."
+    task :symlink do
+      (folders + files).each do |folder|
+        run "rm -rf #{current_path}/#{folder}; ln -s #{shared_path}/#{folder} #{current_path}/#{folder}"
+      end
+    end
+
+  end
+end
+
+after "deploy:setup", "deploy:shared:setup"
+after "deploy:symlink", "deploy:shared:symlink"

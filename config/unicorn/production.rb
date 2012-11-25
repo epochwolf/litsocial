ea39446a221bf57d598e5c29131901f8746145e0
epochwolf <epochwolf@gmail.com>
2012-11-24 16:24:31 -0800

    Fixing rvm issues. RVM has been updated.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,9 +2,9 @@ APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
-    rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-    rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
+    #rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    #rvm_lib_path = File.join(rvm_path, 'lib')
+    #$LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! APP_ROOT
   rescue LoadError

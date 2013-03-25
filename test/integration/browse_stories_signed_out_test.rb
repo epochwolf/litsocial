d74c5ff645c09b401077663860e777ab12fa40ea
epochwolf <epochwolf@gmail.com>
2013-03-24 20:32:02 -0700

    Expanding story integration tests to cover logged out users.

--- test/integration/browse_stories_signed_out_test.rb
+++ test/integration/browse_stories_signed_out_test.rb
@@ -3,30 +3,88 @@ require 'test_helper'
 class BrowseStoriesSignedOutTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # setup do 
-  #   sign_out
-  # end
-
-  # test "Member sees published post on index" do
-  #   get "/news"
-  #   assert response.body =~ /MyPublishedPost/ 
-  # end
-
-  # test "Member doesn't see non-published post on index" do
-  #   get "/news"
-  #   assert response.body !~ /MyHiddenPost/ 
-  # end
+  setup do 
+    sign_out
+  end
 
 
-  # test "Member can access the published posts" do
-  #   get "/news/1"
-  #   assert_response :success
-  # end
+  # Other user's published story
 
-  # test "Member can't access the non-published posts" do
-  #   assert_raise ActiveRecord::RecordNotFound do
-  #     get "/news/2"
-  #   end
-  # end
+  test "Guest sees other user's published story on index" do
+    get "/stories"
+    assert_response 200
+    assert response.body =~ /MembersStory/ 
+  end
+
+  test "Guest sees other user's published story on other user's homepage" do
+    story = stories(:members_story)
+    user = story.user
+    get "/users/#{user.id}"
+    assert_response 200
+    assert response.body =~ /MembersStory/ 
+  end
+
+  test "Guest sees user's published story" do
+    story = stories(:members_story)
+    get "/stories/#{story.id}"
+    assert_response 200
+    assert response.body =~ /MembersStory/ 
+  end
+
+  test "Guest can't edit user's published story" do
+    story = stories(:members_story)
+    get "/stories/#{story.id}/edit"
+    assert_response 302
+  end
+
+
+  # User's locked story
+
+  test "Guest doesn't see user's locked story on index" do
+    story = stories(:locked)
+    get "/stories"
+    assert_response 200
+    assert response.body !~ /BannedsLockedStory/ 
+  end
+
+  test "Guest doesn't see user's locked story on user's homepage" do
+    story = stories(:locked)
+    user = story.user
+    get "/users/#{user.id}"
+    assert_response 200
+    assert response.body !~ /BannedsLockedStory/ 
+  end
+
+  test "Guest doesn't see user's locked story" do
+    story = stories(:locked)
+    get "/stories/#{story.id}"
+    assert_response 403
+    assert response.body !~ /BannedsLockedStory/ 
+  end
+
+
+  # User's deleted story
+
+  test "Guest doesn't see user's deleted story on index" do
+    get "/stories"
+    assert_response 200
+    assert response.body !~ /BannedsDeletedStory/ 
+  end
+
+  test "Guest doesn't see user's deleted story on user's homepage" do
+    story = stories(:deleted)
+    user = story.user
+    get "/users/#{user.id}"
+    assert_response 200
+    assert response.body !~ /BannedsDeletedStory/ 
+  end
+
+  test "Guest doesn't see user's deleted story but can see the title." do
+    story = stories(:deleted)
+    get "/stories/#{story.id}"
+    assert_response 403
+    assert response.body =~ /BannedsDeletedStory/ 
+  end
+
   
 end
